598f124c5014ac81656de67c45b9992e70770e7c
HTL <htluu@ucsd.edu>
2025-07-22 03:04:21 -0700

    control

--- alu.sv
+++ alu.sv
@@ -5,54 +5,41 @@ module alu(
   input[7:0] inA, inB,	 // 8-bit wide datapath
   input[2:0] immediate; //for immediate addition
   input      sc_i,       // shift flag ???????????????????????
-  output logic[7:0] accumulator,
-  output logic[7:0] data, //for moving values between registers
+  
+  output logic[7:0] data_out, //for moving values between registers
   output logic[3:0] regs, //for specifying where to store
-  output logic[1:0] compareFlag, //1X: A = B, 01: A > B, 00: A < B
+  output logic[1:0] compareFlag, //11: no compare, 10: A = B, 01: A > B, 00: A < B
   output logic carry_out,     // shift_carry out
-  //              pari,     // reduction XOR (output)
-	// 		         zero      // NOR (output)
+               halt,
 );
 
-always_comb begin 
-  // accumulator = 'b0;
-  // sc_o = 'b0;
-  // zero = !accumulator;
-  // pari = ^accumulator;
+always_comb begin
+  halt = 0;
+  compareFlag = 2'b11;
 
   case(instruction)
-    0: accumulator = inA & inB; //bitwise and
-    1: accumulator = inA | inB; //bitwise or
-    2: {carry_out, accumulator} = inA + inB + sc_i; // add 2 8-bit unsigned; automatically makes carry-out
-    3: {carry_out, accumulator} = inA - inB + sc_i;
-    4: {carry_out, accumulator} = inA + immediate + sc_i; //add with intermediate
-    5: data = dataA; //how to move between registers?
-    6: //???
-    7: compareFlag = {dataA == dataB, (dataA - dataB) > 0}; //compare dataA with dataB
-    8: {carry_out, accumulator} = {inA, sc_i ? {dataA, 0} : {0, dataA}}; //shift 
+    0: data_out = inA & inB; //bitwise and
+    1: data_out = inA | inB; //bitwise or
+    2: {carry_out, data_out} = inA + inB + sc_i; // add 2 8-bit unsigned; automatically makes carry-out
+    3: {carry_out, data_out} = inA - inB + sc_i;
+    4: {carry_out, data_out} = inA + immediate + sc_i; //add with intermediate
+    5: data_out = dataA; // copy data in 
+    6: data_out = dataA; // copy data in
+    7: begin
+      data_out = '1;
+      compareFlag = {dataA == dataB, (dataA - dataB) > 0}; //compare dataA with dataB
+    end
+    8: {carry_out, data_out} = {inA, sc_i ? {dataA, 0} : {0, dataA}}; // shift by 1 only?
+    // 9: // branch
+    // 10: // beq
+    // 11: // bgt
+    // 12: // blt
+    13: data_out = dataA;
+    // 14: // load
+    15: {halt, data_out} = '1; // halt
     default:
+      data_out = '1;
 
-    // 0: // add 2 8-bit unsigned; automatically makes carry-out
-    //   {sc_o, accumulator} = inA + inB + sc_i;
-    // 1: // left_shift
-    //   {sc_o, accumulator} = {inA, sc_i};
-    //     /*begin
-    //   rslt[7:1] = ina[6:0];
-    //   rslt[0]   = sc_i;
-    //   sc_o      = ina[7];
-    //     end*/
-    // 2: // right shift (alternative syntax -- works like left shift
-    //   {accumulator, sc_o} = {sc_i,inA};
-    // 3: // bitwise XOR
-    //   accumulator = inA ^ inB;
-    // 4: // bitwise AND (mask)
-    //   accumulator = inA & inB;
-    // 5: // left rotate
-    //   accumulator = {inA[6:0],inA[7]};
-    // 6: // subtract
-    //   {sc_o, accumulator} = inA - inB + sc_i;
-    // 7: // pass A
-    //   accumulator = inA;
   endcase
 end
    
